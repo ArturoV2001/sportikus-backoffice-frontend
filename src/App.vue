461aab0d36fe4efaf43b7cb94c7010d74169b3dc
ArturoV2001 <ArturoV2001@outlook.com>
2024-11-24 01:35:16 -0600

    App file fixed

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <template>
-  <LoginComponent v-if="viewOption === 0"/>
-  <MainComponent v-if="viewOption === 1"/>
+  <RouterView/>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { authCheckSession } from '@/services/auth.js';
-import LoginComponent from '@/components/Login/LoginComponent.vue'
-import MainComponent from '@/components/MainComponent.vue'
 
 const auth = ref(false);
 const router = useRouter();
 const response = ref();
-const viewOption = ref();
 
 const startApplication = async () => {
-  viewOption.value =  0;
   auth.value = false;
   if (localStorage.getItem('auth')) {
     response.value = await authCheckSession()
@@ -25,13 +20,12 @@ const startApplication = async () => {
       });
     if (response.value.authenticated === true) {
       localStorage.setItem('user', JSON.stringify(response.value.user));
-      viewOption.value = 1;
+      router.push({ name: 'home' });
     } else {
-      //console.log("llega aqui");
-      //router.push({ name: 'login' });
+      router.push({ name: 'login' });
     }
   } else {
-    //router.push({ name: 'login' });
+    router.push({ name: 'login' });
   }
 };
 
