585337731d3aec2a51ea6fd161f38bb6b39c3d82
ArturoV2001 <ArturoV2001@outlook.com>
2024-11-24 16:59:33 -0600

    dashboard created

---
+++ src/components/Dashboard/DashboardView.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1 class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
+      Bienvenido a Sportikus {{user.name}}!
+    </h1>
+</template>
+
+<script setup>
+
+import { onMounted, ref } from 'vue'
+
+const user = ref([]);
+
+onMounted(() => {
+  user.value = JSON.parse(localStorage.getItem('user'));
+})
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginComponent from '@/components/Login/LoginComponent.vue'
 import MainComponent from '@/components/MainComponent.vue'
+import DashboardView from '@/components/Dashboard/DashboardView.vue'
+
+const mainComponentRoutes = [
+  {
+    path: 'dashboard',
+    name: 'dashboard',
+    components: {
+      mainComponent: DashboardView,
+    },
+  },
+];
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: MainComponent,
+    children: mainComponentRoutes,
   },
   {
     path: '/login',
@@ -19,6 +31,7 @@ const routes = [
     component: () => import('../views/AboutView.vue')
   },
 ];
+
 
 const router = createRouter({
   history: createWebHistory(),
